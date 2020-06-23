5d30e7faac8397fa7f3cb5b7ab31844f849922cc
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-06-23 17:52:01 +0200

    TP Nettoyage : ex1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,27 @@
+package ex1;
+
+/**
+ * Classe qui réalise des opérations arythmétiques
+ * @author DIGINAMIC
+ */
+public class Calcul {
+
+	/** Méthode qui retourne l'addition des entiers a et b 
+	 * 
+	 * @param a int
+	 * @param b int
+	 * @return int resultat de l'addition
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+	
+	/** Méthode qui retourne la soustraction des entiers a et b
+	 * @param a int
+	 * @param b int
+	 * @return int résultat de la soustraction
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
--- src/main/java/ex1/Entreprise.java
+++ src/main/java/ex1/Entreprise.java
@@ -2,7 +2,7 @@ package ex1;
 
 import java.util.Date;
 
-public class entreprise {
+public class Entreprise {
 
 	public int Siret;
 	public String Nom;
